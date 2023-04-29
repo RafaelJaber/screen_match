9ab9fd563ca63dfed0afcc3e8eb50e096373db89
Rafael Jáber <rafael.jaber@hotmail.com>
2023-04-29 01:11:19 -0300

    Controlando a visibilidade do código e definindo os atributos como private e configurado os setters e getters

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import br.com.alura.screenmatch.models.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie movie = new Movie();
-        movie.name = "O Protetor";
-        movie.releaseDate = 2014;
-        movie.durationInMinutes = 140;
+        movie.setName("O Protetor");
+        movie.setReleaseDate(2014);
+        movie.setDurationInMinutes(140);
 
         movie.displayTechnicalSheet();
         movie.evaluate(5.8);
@@ -11,5 +13,7 @@ public class Main {
         movie.evaluate(3.2);
 
         System.out.println("Média das avaliações: " + movie.getAverageRatings());
+        System.out.println("Total avaliações: " + movie.getTotalRatings());
+        System.out.println("Soma das avaliações: " + movie.getSumOfRatings());
     }
 }
